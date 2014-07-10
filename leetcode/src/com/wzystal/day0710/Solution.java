88cc912eb2a808e821ff2df714e72fde937e9011
Young Z. Wang <wzystal@gmail.com>
2014-07-10 15:42:42 +0800

    LeetCode | Single Number

--- leetcode/src/com/wzystal/day0710/Solution.java
+++ leetcode/src/com/wzystal/day0710/Solution.java
@@ -1,75 +1,16 @@
 package com.wzystal.day0710;
 
-import java.util.HashMap;
-
 /**
- * Copy List with Random Pointer
+ * LeetCode | Single Number
+ * 
  * @author wzystal
  */
 public class Solution {
-	class RandomListNode {
-		int label;
-		RandomListNode next, random;
-
-		public RandomListNode(int x) {
-			this.label = x;
+	public int singleNumber(int[] A) {
+		int x = 0;
+		for (int i : A) {
+			x = x ^ i;
 		}
-	}
-
-	// 时间复杂度O(n),空间复杂度O(n)
-	public RandomListNode copyRandomList1(RandomListNode head) {
-		if (head == null)
-			return head;
-		HashMap<RandomListNode, RandomListNode> map = new HashMap<>();
-		RandomListNode newHead = new RandomListNode(head.label);
-		map.put(head, newHead);
-		RandomListNode pre = newHead;
-		RandomListNode node = head.next;
-		while (node != null) {
-			RandomListNode newNode = new RandomListNode(node.label);
-			map.put(node, newNode);
-			pre.next = newNode;
-			pre = pre.next;
-			node = node.next;
-		}
-		node = head;
-		RandomListNode copyNode = newHead;
-		while (node != null) {
-			copyNode.random = map.get(node.random);
-			copyNode = copyNode.next;
-			node = node.next;
-		}
-		return newHead;
-	}
-
-	// 时间复杂度O(n),空间复杂度O(1)
-	public RandomListNode copyRandomList2(RandomListNode head) {
-		if (head == null)
-			return head;
-		RandomListNode node = head;
-		while (node != null) {
-			RandomListNode newNode = new RandomListNode(node.label);
-			newNode.next = node.next;
-			node.next = newNode;
-			node = newNode.next;
-		}
-		node = head;
-		while (node != null) {
-			if (node.random != null) {
-				node.next.random = node.random.next;
-			}
-			node = node.next.next;
-		}
-		RandomListNode newHead = head.next;
-		node = head;
-		while (node != null) {
-			RandomListNode newNode = node.next;
-			node.next = newNode.next;
-			if (newNode.next != null) {
-				newNode.next = newNode.next.next;
-			}
-			node = node.next;
-		}
-		return newHead;
+		return x;
 	}
 }
